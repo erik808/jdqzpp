b93c82dd96950904c7245074f2e482bbc458bab4
Erik Mulder <t.e.mulder@uu.nl>
2016-04-11 16:54:49 +0200

    added a lot of parameters, factorized the setting of the indices/pointers to things in the workspace:

--- src/test.C
+++ src/test.C
@@ -1,37 +1,48 @@
 #include "gtest/gtest.h" // google test framework
 #include "jdqz.H"
 #include <vector>
+#include <complex>
 
-typedef std::vector<double> vector;
+typedef std::vector<std::complex<double> > vector;
 
 //------------------------------------------------------------------
+// Example matrix wrapper
 class TestMatrix
 {
 public:
-	TestMatrix(){};
+	// Define a Vector type
+	using Vector = std::vector<std::complex<double> >;
+
+private:	
+	// Problem size
+	size_t n_;
+	
+public:
+	TestMatrix(int size) : n_(size) {};
 
 	// Subroutine to compute r = Aq
-	void AMUL(size_t n, vector const &q, vector &r)
+	void AMUL(Vector const &q, Vector &r)
 		{
 			// being careful with 0-based indexing
-			for (size_t i = 1; i <= n; ++i)
+			for (size_t i = 1; i <= n_; ++i)
 				r[i-1] = ((double) i) * q[i-1];
 		}
 
 	// Subroutine to compute r = Bq
-	void BMUL(size_t n, vector const &q, vector &r)
+	void BMUL(Vector const &q, Vector &r)
 		{
-			for (size_t i = 1; i <= n; ++i)
+			for (size_t i = 1; i <= n_; ++i)
 				r[i-1] =  q[i-1] / ((double) i);
 		}
 
 	// Subroutine to compute q = K^-1 q
 	//   here we use that the target in JDQZ is 31
-	void PRECON(size_t n, vector &q)
+	void PRECON(Vector &q)
 		{
-			for (size_t i = 1; i <= n; ++i)
+			for (size_t i = 1; i <= n_; ++i)
 				q[i-1] = ((double) i) * q[i-1] / ((double) i*i - 31);
 		}
+	size_t size() { return n_; }
 };
 
 //------------------------------------------------------------------
@@ -42,21 +53,24 @@ TEST(Matrix, Operators)
 	vector vec2(n, 0.0);
 	vector vec3(n, 0.0);
 		
-	TestMatrix testmat;
+	TestMatrix testmat(n);
 
-	testmat.AMUL(n, vec1, vec2);
-	EXPECT_EQ(vec2[49], 50);
-	EXPECT_EQ(vec2[77], 78);
-	testmat.BMUL(n, vec2, vec3);
+	testmat.AMUL(vec1, vec2);
+	EXPECT_EQ(vec2[49].real(), 50);
+	EXPECT_EQ(vec2[77].real(), 78);
+	testmat.BMUL(vec2, vec3);
 	EXPECT_EQ(vec1 == vec3, true);
-	testmat.PRECON(n, vec1);
+	testmat.PRECON(vec1);
 	EXPECT_EQ(vec1[9], (double) 10 / (100-31));
 }
 
 //------------------------------------------------------------------
 TEST(JDQZ, Setup)
 {
-	// TODO
+	size_t size = 100;
+	TestMatrix testmat(size);
+	JDQZ<TestMatrix> jdqz(testmat);
+	EXPECT_EQ(jdqz.size(), size);
 }
 
 //------------------------------------------------------------------
@@ -67,4 +81,3 @@ int main(int argc, char **argv)
 	std::cout << "TEST exit code " << out << std::endl;
 	return out;
 }
-
